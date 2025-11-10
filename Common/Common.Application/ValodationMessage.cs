8a9521d63b94271838d7d54244535b307f4ffc9c
AtaHoseini <ata.hoseini@gmail.com>
2025-11-10 20:16:05 +0000

    UseCases

---
+++ Common/Common.Application/ValodationMessage.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Application
+{
+    public class ValodationMessage
+    {
+        public const string RecaptchError  = "Don't valid recaptcha";
+        public const string Required = "This field is required";
+
+        public static string required(string field)
+        {
+            return $"{field} {Required}";
+        }
+
+        public static string maxKenght(string filed, int maxLenght)
+        {
+            return $"{filed} field must be less than {maxLenght} characters";
+        }
+
+        public static string minKenght(string filed, int minLenght)
+        {
+            return $"{filed} field must be more than {minLenght} characters";
+        }
+    }
+
+}
